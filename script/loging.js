abeeaf78a5cf3e983b95b82b487ecd78a9e11355
afraimkhan18-create <afraimkhan18@gmail.com>
2026-04-29 02:29:26 +0600

    fix: home page redirect path

--- script/loging.js
+++ script/loging.js
@@ -15,7 +15,7 @@ document.getElementById("login-btn").addEventListener("click",function () {
     if (Username === "admin" && Password === "admin123") {
         // 3.1- true alert :::> homepage
         alert('your are success')
-        window.location.assign("home.html")
+        window.location.assign("/Issue-Tracker/home.html")
     }
     //  3.2- false alert :::> return
     else{
